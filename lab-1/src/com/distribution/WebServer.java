724529cbf1b5d921d63dde75520f8c193899504e
KlevcevichAV <alexandr.klevcevich@gmail.com>
2021-02-07 17:32:13 +0300

    feat: added log and simple server

---
+++ lab-1/src/com/distribution/WebServer.java
@@ -0,0 +1,86 @@
+package com.distribution;
+
+import com.distribution.config.Config;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.logging.LogManager;
+import java.util.logging.Logger;
+
+public class WebServer {
+    static Logger LOGGER;
+
+    static {
+        try (FileInputStream ins = new FileInputStream(Config.LOG_CONFIG_FILE_PATH)) {
+            LogManager.getLogManager().readConfiguration(ins);
+            LOGGER = Logger.getLogger(WebServer.class.getName());
+        } catch (Exception ignore) {
+            ignore.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) throws Throwable {
+        try {
+            ServerSocket serverSocket = new ServerSocket(8080);
+            LOGGER.info("Server started on port 8080");
+            while (true) {
+                Socket socket = serverSocket.accept();
+                LOGGER.info("Client accepted");
+                new Thread(new SocketProcessor(socket)).start();
+            }
+        }finally {
+            LOGGER.info("Server processing finished");
+        }
+    }
+
+    private static class SocketProcessor implements Runnable {
+
+        private final Socket socket;
+        private final InputStream input;
+        private final OutputStream output;
+
+        private SocketProcessor(Socket s) throws Throwable {
+            this.socket = s;
+            this.input = s.getInputStream();
+            this.output = s.getOutputStream();
+        }
+
+        public void run() {
+            try {
+                readInputHeaders();
+                writeResponse("<html><body><h1>Hello from Habrahabr</h1></body></html>");
+            } catch (Throwable t) {
+                /*do nothing*/
+            } finally {
+                try {
+                    socket.close();
+                } catch (Throwable t) {
+                    /*do nothing*/
+                }
+            }
+            LOGGER.info("Client processing finished");
+        }
+
+        private void writeResponse(String s) throws Throwable {
+            String response = "HTTP/1.1 200 OK\r\n" +
+                    "Server: YarServer/2009-09-09\r\n" +
+                    "Content-Type: text/html\r\n" +
+                    "Content-Length: " + s.length() + "\r\n" +
+                    "Connection: close\r\n\r\n";
+            String result = response + s;
+            output.write(result.getBytes());
+            output.flush();
+        }
+
+        private void readInputHeaders() throws Throwable {
+            BufferedReader br = new BufferedReader(new InputStreamReader(input));
+            while (true) {
+                String s = br.readLine();
+                if (s == null || s.trim().length() == 0) {
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ lab-1/src/com/distribution/config/Config.java
@@ -0,0 +1,6 @@
+package com.distribution.config;
+
+public class Config {
+    public static final String LOG_CONFIG_FILE_PATH = System.getProperty("user.dir") + "/log/log.config";
+    public static final String DIRECTORY_PATH = System.getProperty("user.home");
+}
