ba546aa4aacd68b3427d7a487757b4ec1a7b0f66
Klevcevich Alex <alexandr.klevcevich@gmail.com>
2021-02-08 20:06:31 +0300

    feat: added request processing (template) and several function templates

---
+++ lab-1/src/main/java/com/distribution/Constant.java
@@ -0,0 +1,19 @@
+package com.distribution;
+
+public class Constant {
+    public static final String LOG_CONFIG_FILE_PATH = System.getProperty("user.dir") + "/log/log.config";
+    public static final String DIRECTORY_PATH = System.getProperty("user.home");
+    public static final String RESOURCES_PATH = System.getProperty("user.dir") + "/src/main/resources";
+
+    public static final String GET = "GET";
+    public static final String POST = "POST";
+    public static final String OPTIONS = "OPTIONS";
+
+
+    public static final String CODE_200 = "200 OK";
+    public static final String CODE_403 = "403 Forbidden";
+    public static final String CODE_404 = "404 Not found";
+    public static final String CODE_501 = "501 Not implemented";
+
+
+}
--- lab-1/src/main/java/com/distribution/WebServer.java
+++ lab-1/src/main/java/com/distribution/WebServer.java
@@ -1,7 +1,5 @@
 package com.distribution;
 
-import com.distribution.config.Config;
-
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -12,18 +10,21 @@ import java.nio.file.Paths;
 import java.util.logging.LogManager;
 import java.util.logging.Logger;
 
+import static com.distribution.Constant.*;
+
 public class WebServer {
 
     private static Logger LOGGER;
 
     static {
-        try (FileInputStream ins = new FileInputStream(Config.LOG_CONFIG_FILE_PATH)) {
+        try (FileInputStream ins = new FileInputStream(LOG_CONFIG_FILE_PATH)) {
             LogManager.getLogManager().readConfiguration(ins);
             LOGGER = Logger.getLogger(WebServer.class.getName());
         } catch (Exception ignore) {
             ignore.printStackTrace();
         }
     }
+
 
     private static String[] parts;
 
@@ -68,13 +69,36 @@ public class WebServer {
     }
 
     private static void writeResponse(PrintWriter output) throws IOException {
-        Path path = Paths.get(Config.RESOURCES_PATH, parts[1]);
+        switch (parts[0]) {
+            case GET: {
+                getRequestProcessing(output);
+                break;
+            }
+            case POST: {
+                postRequestProcessing(output);
+                break;
+            }
+            case OPTIONS: {
+                optionsRequestProcessing(output);
+                break;
+            }
+            default: {
+                output.println("HTTP/1.1 501 NOT_IMPLEMENTED");
+                output.println("Content-Type: text/html; charset=utf-8");
+                output.println();
+                output.println("<h1>Not Implemented</h1>");
+                output.flush();
+                LOGGER.warning("501 NOT IMPLEMENTED");
+            }
+        }
+        Path path = Paths.get(RESOURCES_PATH, parts[1]);
         if (!Files.exists(path)) {
             output.println("HTTP/1.1 404 NOT_FOUND");
             output.println("Content-Type: text/html; charset=utf-8");
             output.println();
             output.println("<h1>File not found!</h1>");
             output.flush();
+            LOGGER.warning("404 NOT FOUND");
             return;
         }
 
@@ -83,5 +107,36 @@ public class WebServer {
         output.println();
 
         Files.newBufferedReader(path).transferTo(output);
+        LOGGER.info("200 OK");
+    }
+
+    private static void getRequestProcessing(PrintWriter output) {
+
+    }
+
+    private static void postRequestProcessing(PrintWriter output) {
+
+    }
+
+    //    > OPTIONS /api/strings/ HTTP/1.1
+//            > Host: localhost:8080
+//            > User-Agent: curl/7.52.1
+//            > Accept: */*
+//>
+//< HTTP/1.1 200
+//< Allow: GET,HEAD
+//< Content-Length: 0
+//< Date: Tue, 01 May 2018 16:40:21 GMT
+    private static void optionsRequestProcessing(PrintWriter output) {
+
+    }
+
+    private static void write(PrintWriter output, Path path, String result) throws IOException {
+        output.println("HTTP/1.1 " + result);
+        output.println("Content-Type: text/html; charset=utf-8");
+        output.println();
+
+        Files.newBufferedReader(path).transferTo(output);
+        LOGGER.info(result);
     }
 }
